203b332a76501738423ec594fca61061843966aa
402882 <402882@superai.aiat>
2024-10-27 22:59:24 +0700

    Create new Next

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {reactStrictMode: true,};
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'export'
+};
 
 export default nextConfig;
-
